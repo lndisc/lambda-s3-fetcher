5e71a41d2a92fc532e81d8cd8f178d33a1ab3900
Isaac Morcillo <lndisc54@gmail.com>
2024-06-02 20:15:10 +0100

    add archive provider

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "~> 2.4.0"
+    }
   }
   backend "s3" {
     bucket         	   = "eg-dev-tf-lambda-s3-fetcher"
