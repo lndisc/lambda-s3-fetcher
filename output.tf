863550b7cd701c861c39d039dfb6c20f784c7102
Isaac Morcillo <lndisc54@gmail.com>
2024-06-02 21:15:52 +0100

    test

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+# Output the result of the Lambda invocation
 output "result_entry" {
-  value = jsondecode(data.aws_lambda_invocation.invoke_lambda.result)["key1"]
+  value = jsondecode(data.aws_lambda_invocation.invoke_lambda.result)["body"]
 }
